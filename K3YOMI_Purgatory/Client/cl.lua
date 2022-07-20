89aac08f112a83fbc3a58ce1f5bf0baa43b43803
Kiyomi <54733885+K3YOMI@users.noreply.github.com>
2022-07-20 06:57:27 -0500

    Add files via upload

---
+++ K3YOMI_Purgatory/Client/cl.lua
@@ -0,0 +1,33 @@
+
+
+-- Also if you are a cheating reading this, you can change the config to your liking but the server uses this mostly. lol
+_config = {}
+_config.inviteDiscord = "https://discord.gg/XXXXX" -- Discord invite link
+_config.ForceDiscordInvite = true -- If true, the player will be forced to show an invite on their discord client to the directed discord server
+
+
+
+RegisterNetEvent('_ForcePunishment')
+AddEventHandler('_ForcePunishment', function(__punishmentSecond, _reason, staffmember)
+	SendNUIMessage({type = 'open',seconds = tonumber(__punishmentSecond),reason = _reason, staff = staffmember})
+end)
+
+RegisterNetEvent('_ForceRemovePunishment')
+AddEventHandler('_ForceRemovePunishment', function()
+	SendNUIMessage({type = 'close'})
+end)
+
+RegisterNUICallback('_CloseRules', function(data, cb)
+	SendNUIMessage({type = 'close'})
+end)
+function _autoInvite()
+    if _config.ForceDiscordInvite then
+        local txd = CreateRuntimeTxd("duiTxd1")
+        local duiObj = CreateDui(_config.inviteDiscord, 1024, 512)
+        local dui = GetDuiHandle(duiObj)
+        local tx = CreateRuntimeTextureFromDuiHandle(txd, "duiTex", dui)
+        AddReplaceTexture("cs4_09_bilbrd_01", "bills_foreclosure_billboard_1024", "duiTxd1", "duiTex")
+    end
+end
+_autoInvite()
+
---
+++ K3YOMI_Purgatory/fxmanifest.lua
@@ -0,0 +1,8 @@
+author 'K3YOMI@Github'
+description '[K3YOMI] Simple auto-discord invite and force punishment system'
+game 'common'
+fx_version 'adamant'
+server_scripts {'Server/*.*'}
+client_scripts {'Client/*.*'}
+ui_page 'Index/index.html'
+files {'Index/*.*'}
