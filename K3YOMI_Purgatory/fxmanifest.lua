9db59089c4a2ead101c0d962649c126553f334bd
Kiyomi <54733885+K3YOMI@users.noreply.github.com>
2022-09-11 11:53:18 -0500

    v1 - Removing

--- K3YOMI_Purgatory/fxmanifest.lua
+++
@@ -1,8 +0,0 @@
-author 'K3YOMI@Github'
-description '[K3YOMI] Simple auto-discord invite and force punishment system /timeout'
-game 'common'
-fx_version 'adamant'
-server_scripts {'Server/*.*'}
-client_scripts {'Client/*.*'}
-ui_page 'Index/index.html'
-files {'Index/*.*'}
